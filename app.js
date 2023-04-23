1c7ff544a990e5c9a280cf17b0ab9223477e6608
Marin Ljuban <marin.ljuban@gmail.com>
2023-04-23 22:56:12 +0100

    Added possibility of querying for a string typed in manually

--- app.js
+++ app.js
@@ -4,43 +4,80 @@ import namespace from '@rdfjs/namespace';
 import SparqlClient from 'sparql-http-client'
 
 //Create the namespace object
-const ns = {
-  schema: namespace('http://schema.org/'),
-  rdf: namespace('http://www.w3.org/1999/02/22-rdf-syntax-ns#'),
-  bot: namespace('https://w3id.org/bot#')
-};
-
+const ifc4 = namespace('http://ifcowl.openbimstandards.org/IFC4_ADD2#');
+const otl = namespace('https://otl.buildingsmart.org/IFC4_ADD2_TC1/def/');
 
 //Get the data from local endpoint
-const client = new SparqlClient({ endpointUrl: 'http://DESKTOP-SQ747CJ:7200/repositories/IFC4_ADD2_RDF' })
+const client = new SparqlClient({ endpointUrl: 'http://DESKTOP-SQ747CJ:7200/repositories/Ifc4Test' })
+
 
 //Create the SPARQL Query
-const stream = await client.query.select(`
-PREFIX nen2660: <https://w3id.org/nen2660/def#>
-PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC4_ADD2#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-PREFIX expr: <https://w3id.org/express#>
-PREFIX zh: <https://w3id.org/ziekenhuis/def#>
-PREFIX owl: <http://www.w3.org/2002/07/owl#>
-PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
-select DISTINCT ?enumtype where { 
-?enum		 rdf:type ?enumtype .
-    ?enumtype	?p expr:ENUMERATION .
-    
-    FILTER (?p = rdfs:subClassOf)
-    FILTER(?enum != ifc:NOTDEFINED)
-    FILTER(?enum != ifc:USERDEFINED)
+async function subclassQuery(superclass) {
+  const stream = await client.query.select(`
+  PREFIX nen2660: <https://w3id.org/nen2660/def#>
+  PREFIX ifc4: <http://ifcowl.openbimstandards.org/IFC4_ADD2#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX expr: <https://w3id.org/express#>
+  PREFIX zh: <https://w3id.org/ziekenhuis/def#>
+  PREFIX owl: <http://www.w3.org/2002/07/owl#>
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
 
-} limit 2000
+  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
-`)
+  SELECT ?directSub
+  WHERE { ?directSub rdfs:subClassOf ifc4:Ifc${superclass} 
+  }
+
+  `)
+  //Write the data to console
+  stream.on('data', row => {
+    console.log(row.directSub.value.slice(45))
+  })
+}
+
+async function enumQuery(superclass) {
+  const enumStream = await client.query.select(`
+  PREFIX nen2660: <https://w3id.org/nen2660/def#>
+  PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC4_ADD2#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX expr: <https://w3id.org/express#>
+  PREFIX zh: <https://w3id.org/ziekenhuis/def#>
+  PREFIX owl: <http://www.w3.org/2002/07/owl#>
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+
+  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+  select ?enumtype ?enum where { 
+  ?enum		 rdf:type ?enumtype .
+      ?enumtype	?p expr:ENUMERATION .
+      
+      FILTER (?enumtype = ifc:Ifc${superclass}TypeEnum)
+      FILTER (?p = rdfs:subClassOf)
+      FILTER(?enum != ifc:NOTDEFINED)
+      FILTER(?enum != ifc:USERDEFINED)
+
+  } limit 2000
 
 
-//Write the data to console
-stream.on('data', row => {
-  console.log(row)
-})
+  `)
+
+  enumStream.on('data', row => {
+    console.log(row.enum.value.slice(45))
+  })
+}
 
 
+const queriedElement = 'Door'
+
+// console.log(subclassQuery(queriedElement)); 
+console.log(enumQuery(queriedElement)); 
+
+// async function sparqlQuery(queriedItem) {
+  
+//   subclassQuery(queriedItem);
+
+//   enumQuery(queriedItem);
+
+//   }
+
+//   sparqlQuery('Window');
