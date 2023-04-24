d326f028bd83cbb6a569d273f858d55a3d9de306
Marin Ljuban <marin.ljuban@gmail.com>
2023-04-24 11:26:32 +0100

    Push

--- app.js
+++ app.js
@@ -9,13 +9,32 @@ import rdf from 'rdf-ext';
 const ifc4 = namespace('http://ifcowl.openbimstandards.org/IFC4_ADD2#');
 // const otl = namespace('https://otl.buildingsmart.org/IFC4_ADD2_TC1/def/');
 
-//GET THE DATA FROM LOCAL ENDPOINT
+//CREATE LOCAL SPARQL ENDPOINT
 const client = new SparqlClient({ endpointUrl: 'http://DESKTOP-SQ747CJ:7200/repositories/Ifc4Test' })
 
 
+//CREATE THE ARRAY OF FIRST ITEMS TO BE QUERIED
+const queriedElements = ['DistributionElement', 'FurnishingElement', 'BuildingElement', 'ElementComponent'];
 
-const queriedElement = 'Sensor'
-//CREATE THE SPARQL QUERIES
+console.log(Array.isArray(queriedElements));
+
+//LOGIC FUNCTION
+queriedElements.forEach(function (item) {
+  const foundItems = subclassQuery(item);
+  console.log(foundItems);
+  // if (foundItems.length > 0) {
+  //   foundItems.forEach(function(foundItem) {
+  //     subclassQuery(foundItem);
+  //   });
+  // } else {
+  //   foundItems.forEach(function(foundItem) {
+  //     enumQuery(foundItem);
+  //   });
+  // }
+});
+
+
+//CREATE THE SPARQL SUBCLASS QUERY
 async function subclassQuery(superclass) {
   const stream = await client.query.select(`
   PREFIX nen2660: <https://w3id.org/nen2660/def#>
@@ -25,13 +44,11 @@ async function subclassQuery(superclass) {
   PREFIX zh: <https://w3id.org/ziekenhuis/def#>
   PREFIX owl: <http://www.w3.org/2002/07/owl#>
   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-
   PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
   SELECT ?directSub
   WHERE { ?directSub rdfs:subClassOf ifc4:Ifc${superclass} 
   }
-
   `)
   //CREATE AN ARRAY OF OBJECTS WITH THE QUERY RESULTS - OVO MI TREBA
   stream.on('data', row => {
@@ -40,6 +57,7 @@ async function subclassQuery(superclass) {
 }
 
 
+//CREATE THE SPARQL ENUM QUERY
 async function enumQuery(superclass) {
   const enumStream = await client.query.select(`
   PREFIX nen2660: <https://w3id.org/nen2660/def#>
@@ -106,5 +124,5 @@ async function enumQuery(superclass) {
 //   sparqlQuery('Window');
 
 
-subclassQuery(`${queriedElement}`);
-enumQuery(`${queriedElement}`);
+// subclassQuery(`${queriedElement}`);
+// enumQuery(`${queriedElement}`);
