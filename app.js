cb9bf83088ddae74d3eb6a079119bfc63f699bee
Marin Ljuban <marin.ljuban@gmail.com>
2023-04-27 13:48:03 +0100

    Push

--- app.js
+++ app.js
@@ -1,26 +1,38 @@
 //IMPORT DEPENDENCIES
-import * as fs from 'fs';
-import namespace from '@rdfjs/namespace';
-import SparqlClient from 'sparql-http-client';
-import cf from 'clownface';
-import rdf from 'rdf-ext';
+import * as fs from "fs";
+import namespace from "@rdfjs/namespace";
+import SparqlClient from "sparql-http-client";
+import cf from "clownface";
+import rdf from "rdf-ext";
+import { create } from "domain";
+import toFile from "rdf-utils-fs";
+import { readFileSync } from "fs";
 
 //CREATE THE NAMESPACES
-// const ifc4 = namespace('http://ifcowl.openbimstandards.org/IFC4_ADD2#');
+// const ifc = namespace('http://ifcowl.openbimstandards.org/IFC4_ADD2#');
 // const otl = namespace('https://otl.buildingsmart.org/IFC4_ADD2_TC1/def/');
 
 //CREATE LOCAL SPARQL ENDPOINT
-const client = new SparqlClient({ endpointUrl: 'http://DESKTOP-SQ747CJ:7200/repositories/Ifc4Test' })
+const client = new SparqlClient({
+  endpointUrl: "http://DESKTOP-SQ747CJ:7200/repositories/Ifc4Test",
+});
 
+//CREATE GRAPH
+const otl = cf({ dataset: rdf.dataset() });
 
 //CREATE THE ARRAY OF FIRST ITEMS TO BE QUERIED
-const queriedElements = ['BuildingElement', 'DistributionElement', 'FurnishingElement', 'ElementComponent'];
+const queriedElements = [
+  "BuildingElement",
+  "DistributionElement",
+  "FurnishingElement",
+  "ElementComponent",
+];
 
-//CREATE THE SPARQL SUBCLASS QUERY
- async function subclassQuery(superclass) {
+//CREATE THE FUNCTION FOR SPARQL QUERY OF CLASSES
+async function subclassQuery(superclass) {
   const classStream = await client.query.select(`
   PREFIX nen2660: <https://w3id.org/nen2660/def#>
-  PREFIX ifc4: <http://ifcowl.openbimstandards.org/IFC4_ADD2#>
+  PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC4_ADD2#>
   PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
   PREFIX expr: <https://w3id.org/express#>
   PREFIX zh: <https://w3id.org/ziekenhuis/def#>
@@ -29,26 +41,27 @@ const queriedElements = ['BuildingElement', 'DistributionElement', 'FurnishingEl
   PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
 
   SELECT ?directSub
-  WHERE { ?directSub rdfs:subClassOf ifc4:Ifc${superclass} 
+  WHERE { ?directSub rdfs:subClassOf ifc:Ifc${superclass} 
   }
-  `)
-  //CREATE AN ARRAY OF OBJECTS WITH THE QUERY RESULTS - THIS DOES NOT WORK!!!!!!
+  `);
+
+  //RETURN AN ARRAY OF OBJECTS WITH THE QUERY RESULTS
   let classArray = [];
 
-  return new Promise(resolve => {
-    classStream.on('data', (row) => {
-      classArray.push(row.directSub.value.slice(48));
-    })
-    .on('end', () => {
-      resolve (classArray);
-    });
-})
+  return new Promise((resolve) => {
+    classStream
+      .on("data", (row) => {
+        classArray.push(row.directSub.value.slice(48));
+      })
+      .on("end", () => {
+        resolve(classArray);
+      });
+  });
 }
 
-
-//CREATE THE SPARQL ENUM QUERY
+//CREATE THE FUNCTION FOR SPARQL QUERY OF ENUMERATIONS
 async function enumQuery(superclass) {
-  
+  //DEFINE SPARQL QUERY
   const enumStream = await client.query.select(`
   PREFIX nen2660: <https://w3id.org/nen2660/def#>
   PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC4_ADD2#>
@@ -69,82 +82,120 @@ async function enumQuery(superclass) {
       FILTER(?enum != ifc:USERDEFINED)
 
   } limit 2000
-  `)
+  `);
 
-  let enumArray = []
-  //CREATE AN ARRAY OF OBJECTS WITH THE QUERY RESULTS - THIS DOES NOT WORK!!!!!!
-
-  return new Promise(resolve => {
-    enumStream.on('data', (row) => {
-      enumArray.push(row.enum.value.slice(45));
-    })
-    .on('end', () => {
-      resolve (enumArray);
-    });
-})
+  //RETURN AN ARRAY OF OBJECTS WITH THE QUERY RESULTS
+  let enumArray = [];
+  return new Promise((resolve) => {
+    enumStream
+      .on("data", (row) => {
+        enumArray.push(row.enum.value.slice(45));
+      })
+      .on("end", () => {
+        resolve(enumArray);
+      });
+  });
 }
-
-
-// const query = await subclassQuery('BuildingElement');
-// console.log(query);
-
-// subclassQuery('BuildingElement').then((enumArray) => {
-//   console.log(enumArray);
-// }).catch((error) => {
-//   console.error(error);
-// });
-
-
 
 //LOGIC FUNCTION
 async function logicFunction(queried) {
-  // remove empty arrays from queried
-
   for (const item of queried) {
     let foundItems = await subclassQuery(item);
-    foundItems = foundItems.filter(Boolean);
-    console.log(foundItems);
     if (foundItems.length > 0) {
+      // console.log(foundItems);
       await logicFunction(foundItems);
     } else {
       let enumItems = await enumQuery(item);
-      enumItems = enumItems.filter(Boolean);
-      console.log(enumItems);
+      if (enumItems.length > 0) {
+        // console.log(enumItems);
+        for (const enumeration of enumItems) {
+          await createNode(enumeration, item);
+        }
+
+        // console.log(enumeration);
+      }
+    }
+  }
+  return otl;
+}
+// }
+
+// logicFunction(queriedElements);
+
+function createNode(enumeration, item) {
+  otl
+    .namedNode(`otl:${enumeration}`)
+    .addOut("a", "owl:Class")
+    .addOut("rdfs:subClassOf", `otl:${item}`);
+}
+
+async function logGraph() {
+  const graphLog = await logicFunction(queriedElements);
+
+  for (const quad of otl.dataset) {
+    console.log(
+      `${quad.subject.value} ${quad.predicate.value} ${quad.object.value}`
+    );
+  }
+}
+
+logGraph();
+
+async function fullQuery(superclass) {
+  const classStream = await client.query.select(`
+  PREFIX nen2660: <https://w3id.org/nen2660/def#>
+  PREFIX ifc: <http://ifcowl.openbimstandards.org/IFC4_ADD2#>
+  PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+  PREFIX expr: <https://w3id.org/express#>
+  PREFIX zh: <https://w3id.org/ziekenhuis/def#>
+  PREFIX owl: <http://www.w3.org/2002/07/owl#>
+  PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+  
+  PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+  SELECT DISTINCT ?subObject ?directParent ?enum
+    WHERE { 
+         ?subObject rdfs:subClassOf* ifc:IfcDistributionElement .
+         ?subObject rdfs:subClassOf ?directParent .
+         OPTIONAL {
+            ?predefinedTypeRelation rdfs:domain ?subObject ;
+                                    rdfs:range ?subObjectEnumType .
+            ?enum rdf:type ?subObjectEnumType .
+            ?subObjectEnumType rdfs:subClassOf expr:ENUMERATION .
+
+            FILTER(?enum != ifc:NOTDEFINED)
+            FILTER(?enum != ifc:USERDEFINED)
+         }
+    }
+  
+  `);
+
+  //RETURN AN ARRAY OF OBJECTS WITH THE QUERY RESULTS
+  let classArray = [];
+
+  return new Promise((resolve) => {
+    classStream
+      .on("data", (row) => {
+        classArray.push(row);
+      })
+      .on("end", () => {
+        resolve(classArray);
+      });
+  });
+}
+
+async function fullLogicFunction(queried) {
+  for (const item of queried) {
+    let foundItems = await fullQuery(item);
+    for (const item of foundItems) {
+      if (item.subObject.value) {
+        console.log(item.directParent);
+      }
     }
   }
 }
 
-logicFunction(queriedElements);
+// fullLogicFunction(queriedElements);
 
+// await toFile(otl.toStream(), 'test.ttl');
 
-
-// //FUNCTION FOR GRAPH CREATION
-// const otl = cf({ dataset: rdf.dataset() });
-
-// otl
-// .namedNode(`otl:${queriedElement}`)
-// .addOut('a', 'owl:Class')
-// .addOut('rdfs:subClassOf', 'otl:BuildingElement');
-
-// for (const quad of otl.dataset) {
-//   console.log(`${quad.subject.value} ${quad.predicate.value} ${quad.object.value}`)
-// }
-
-// console.log(subclassQuery(queriedElement)); 
-// console.log(enumQuery(queriedElement)); 
-
-
-//Create the logic function
-// async function sparqlQuery(queriedItem) {
-  
-//   subclassQuery(queriedItem);
-
-//   enumQuery(queriedItem);
-
-//   }
-
-//   sparqlQuery('Window');
-
-
-// subclassQuery(`${queriedElement}`);
-// enumQuery(`${queriedElement}`);
+// console.log(readFileSync('test.ttl').toString());
